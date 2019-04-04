a52eedf754c9897860214a0f1ae970c13b1da11a
suyufei <yufeisu@live.unc.edu>
2019-04-04 14:04:09 -0400

    Add files via upload

--- ICF_calc_new.py
+++ ICF_calc_new.py
@@ -10,12 +10,8 @@ Created on Tue Mar 12 18:00:52 2019
 import pandas as pd
 import numpy as np
 
-#d=pd.read_excel('PNW_hydro/FCRPS/daily_streamflows.xlsx','Inflows',usecols=[51], names=['0'])[122:-243]
-#d= d.reset_index(drop=True)
-#d=pd.read_csv('PNW_hydro/FCRPS/Climate_models/historical_livneh_VIC_P1-synthetic_streamflows_TDA.csv')
-#d=pd.read_csv('Synthetic_streamflows/synthetic_streamflows_TDA.csv',header=None)
-d=pd.read_csv('Synthetic_streamflows/synthetic_streamflows_FCRPS.csv',usecols=[47],names=['0'],header=None)
 
+d=pd.read_csv('Synthetic_streamflows/synthetic_streamflows_TDA.csv',header=None)
 doy = np.arange(1,366)
 doy_array = np.tile(doy,int(len(d)/365))
 doy_array = pd.DataFrame(doy_array)
